bd9c76f296b4f0aae3aba45cfa25ecc1b900a181
Ioana Laura Ghergu <101597846+ioanaghergu@users.noreply.github.com>
2025-07-08 08:07:08 +0300

    Update UserServiceClient.java

--- movieservice/src/main/java/org/market/movieservice/client/UserServiceClient.java
+++ movieservice/src/main/java/org/market/movieservice/client/UserServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeignClient(name = "userservice", configuration = FeignClientConfig.class)
+@FeignClient(name = "gatewayserver", configuration = FeignClientConfig.class)
 public interface UserServiceClient {
 
     @GetMapping("/api/v1/users/{userId}")
